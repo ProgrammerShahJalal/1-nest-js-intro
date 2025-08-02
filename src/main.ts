5767ce37fdc2bbcdb562fb4c4f307afaee1eb725
ProgrammerShahJalal <you@example.com>
2025-08-02 18:10:43 +0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 8080, '127.0.0.1');
+  console.log('Application is running on http://127.0.0.1:8080');
+}
+
+bootstrap().catch((err) => {
+  console.error('Error starting application:', err);
+  process.exit(1);
+});
